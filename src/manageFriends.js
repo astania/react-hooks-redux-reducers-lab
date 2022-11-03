c9a9b388594b94c3540ead5d33242a136f50fec2
+ <+>
2022-11-03 16:23:21 -0400

    completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
 export function manageFriends(state, action) {
   // your code here
+
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [ ...state.friends, action.payload ] }
+    case "friends/remove":
+      const filteredFriends = state.friends.filter(friend => friend.id != action.payload)
+      return { friends: filteredFriends }
+    default:
+      return state
+
+  }
 }
